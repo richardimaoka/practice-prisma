2b3273a39679613a7dd3f7f097fc92fba43f91f0
richardimaoka <richard.s.imaoka@gmail.com>
2025-08-25 06:17:35 +0900

    script

---
+++ script.ts
@@ -0,0 +1,21 @@
+// .js file extension in the import statement, following:
+//   https://www.prisma.io/docs/orm/prisma-schema/overview/generators?#importing-prisma-client
+// since file extension is mandatory in Node.js runtime:
+//   https://nodejs.org/api/esm.html#mandatory-file-extensions
+import { PrismaClient } from "./generated/prisma/client.js";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  // ... you will write your Prisma Client queries here
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
