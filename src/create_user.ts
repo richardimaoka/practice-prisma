ff7a31a60136871667a1e756a512635d60a169da
richardimaoka <richard.s.imaoka@gmail.com>
2025-11-05 08:38:31 +0900

    create_user.ts barebone

--- src/create_user.ts
+++ src/create_user.ts
@@ -1,7 +1,11 @@
+import "dotenv/config";
 import { PrismaBetterSQLite3 } from "@prisma/adapter-better-sqlite3";
 import { PrismaClient } from "../generated/prisma/client";
 
-const adapter = new PrismaBetterSQLite3({
-  url: process.env.PRISMA_CLIENT_DATABASE_URL,
-});
+const dbPath = process.env.DATABASE_URL;
+if (!dbPath) {
+  throw new Error("Environment variable SSDATABASE_URL not defined!!");
+}
+
+const adapter = new PrismaBetterSQLite3({ url: dbPath });
 const prisma = new PrismaClient({ adapter });
