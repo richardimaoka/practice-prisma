47fcf3495ea2da1e5ce78c53801832b335d0c8c6
richardimaoka <richard.s.imaoka@gmail.com>
2025-11-07 02:34:22 +0900

    done

--- src/create_bio_and_connect_user.ts
+++ src/create_bio_and_connect_user.ts
@@ -13,16 +13,28 @@ const prisma = new PrismaClient({ adapter });
 async function main() {
   const user = await prisma.user.create({
     data: {
-      name: "Bobice",
-      bio: {
-        create: {
-          text: "abcde",
-        },
-      },
+      name: "Manuka",
     },
   });
 
-  console.log("created user:", user);
+  const bio = await prisma.userBio.create({
+    data: {
+      text: "abcdeefgdgd",
+      userId: user.id,
+    },
+  });
+
+  const result = await prisma.user.update({
+    where: {
+      id: user.id,
+    },
+    data: {
+      bio: {
+        connect: {},
+      },
+    },
+  });
+  console.log("created user and connected bio:", result);
 }
 
 main()
