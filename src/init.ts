e3f559396a69ac3bcd9fbd7ee3afa8c99b4171ca
richardimaoka <richard.s.imaoka@gmail.com>
2025-11-20 21:44:05 +0900

    generated and mingrated

---
+++ src/init.ts
@@ -0,0 +1,28 @@
+import "dotenv/config";
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+import { PrismaClient } from "../generated/prisma/client";
+
+const connectionString = `${process.env.DATABASE_URL}`;
+const adapter = new PrismaBetterSqlite3({ url: connectionString });
+const prisma = new PrismaClient({ adapter });
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      name: "Bobice",
+      email: "bobice.bavice@gmail.com",
+    },
+  });
+
+  console.log("created user:", user);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
